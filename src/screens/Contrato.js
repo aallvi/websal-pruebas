ee8365f6bd1f4187ddb74da258bb1aae81c17e2c
Alvaro <aleiva97@gmail.com>
2022-03-31 17:28:43 -0300

    datos y contrato funcionando con scss y waves

--- src/screens/Contrato.js
+++ src/screens/Contrato.js
@@ -13,6 +13,9 @@ export const Contrato = () => {
         
         <div className='headerContrato' >
            <p >Mi Contrato</p> 
+           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 79 1440 320">
+           <path fill="#B3F5FE" fill-opacity="1" d="M0,256L120,256C240,256,480,256,720,234.7C960,213,1200,171,1320,149.3L1440,128L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path>
+        </svg>
         </div>
 
         <GreyLabel primerDato='Empresa' segundoDato='Websal Sistemas de Gestion S.P.A' />
--- src/screens/Datos.js
+++ src/screens/Datos.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { GreyLabel } from '../components/contratoydatos/GreyLabel'
 import { WhiteLabel } from '../components/contratoydatos/WhiteLabel'
 import { Loading } from '../components/Loading';
-
+import wave from '../assets/wave.svg'
 
 export const Datos = () => {
 
@@ -33,7 +33,10 @@ export const Datos = () => {
          <>
 
         <div className='headerContrato' >
-           <p >Mis Datos</p> 
+        <p >Mis Datos</p> 
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 79 1440 320">
+          <path fill="#B3F5FE" fill-opacity="1" d="M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,250.7C1248,256,1344,288,1392,304L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+        </svg>
         </div>
 
         
