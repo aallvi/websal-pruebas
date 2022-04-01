5f71830db4b975bb141283505846c170c91d6124
Alvaro <aleiva97@gmail.com>
2022-04-01 11:38:56 -0300

    empezando la tabla de vacaciones

--- src/screens/Vacaciones.js
+++ src/screens/Vacaciones.js
@@ -8,7 +8,54 @@ export const Vacaciones = () => {
 
 
   return (
-    <div>Vacaciones
+    <div className='titulo' >
+      <p>Informacion sobre tus Vacaciones</p>
+
+      <div className='contenedorDatosPrincipalesVacas' >
+
+              <div className='recuadros' >
+                    <div className='contenedorLeftdata' >
+                          <p>
+                            Inicio Contrato
+
+                          </p>
+
+                    </div>
+
+                    <p className='textoRightData' > 01/08/2020 </p>
+
+              </div>
+
+              <div className='recuadros' >
+                
+                      <div className='contenedorLeftdata' >
+                          <p>
+                            Normales a la Fecha
+                            
+                          </p>
+
+                    </div>
+                    <p className='textoRightData' > 25,1 Dias Habiles </p>
+                 
+ 
+              </div>
+
+              <div className='recuadros' >
+
+                      <div className='contenedorLeftdata' >
+                            <p>
+                            Progresivas a la Fecha
+                              
+                            </p>
+
+                    </div>
+
+                    <p className='textoRightData' > 0 Dias Habiles </p>
+
+
+              </div>
+
+      </div>
 
 
       
